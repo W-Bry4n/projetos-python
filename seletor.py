59b15329bd7c6100364581518bcf52e7196c4faf
W-Bry4n <willianbryan307@gmail.com>
2026-05-04 14:09:18 +0000

    v2

---
+++ seletor.py
@@ -0,0 +1,12 @@
+print("seletor de caractéres🔠")
+print("Escolha uma letra e veja se ela está disponível em nosso sistema⚠️")
+letra = input("digite a letra: ").upper()
+
+if letra == "A":
+  print("Você escolheu a letra A ✅")
+          
+elif letra == "B":
+  print("Você escolheu a letra B ✅")
+
+else:
+  print("Essa opção não está disponível")
