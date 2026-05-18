34c916a9443082c27e7d2a2b1a0f2d0580363384
W-Bry4n <willianbryan307@gmail.com>
2026-05-18 14:18:59 +0000

    v1

--- tabuada.py
+++ tabuada.py
@@ -2,4 +2,4 @@ numero = int(input("tabuada de qual número? "))
 print(f"Tabuada do {numero}")
 for i in range(1, 21):
      resultado = numero * i
-     print(f"{numero} x {i} = {resultado}")
+     print(f"{numero} x {i} = {resultado}") 
