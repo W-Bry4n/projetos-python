10f978627cdc434efa09517d908b202a97406fc3
W-Bry4n <willianbryan307@gmail.com>
2026-06-01 12:53:35 -0300

    Create cuidado.py

---
+++ cuidado.py
@@ -0,0 +1,18 @@
+#---lorran elias da conceição---
+# Função: calcular_imc(p,a)
+#Recebe peso (p) e altura (a), retorna o resultado da conta: peso/altura²
+def calcular_imc(p,a):
+    resultado = p/ (a ** 2)
+    return resultado
+#Davi2: classificador de dados
+def classificar(valor_IMC)
+   if <=25
+      rerturn "normal"
+    else:
+      rerturn "acima do peso"
+#Willian3: Especialista em conteúdo
+def gerar_aviso(status):
+ if status == "normal":
+    return("Parabéns! Seu peso está adequando para sua altura! Mantenha os hábitos saúdaveis, com alimentação saudável e praticando atividades físicas")
+ else:
+    return("Seu IMC indica que você está acima do peso! Recomenda-se buscar uma dieta balenceada e incluir exercícios físicos em sua rotina")
